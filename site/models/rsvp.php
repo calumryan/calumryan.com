5896abb9f9757435febedc46de94917bacc5e98e
Calum Ryan <calumeryan@gmail.com>
2020-11-08 01:45:26 +0000

    Update rsvp class

--- site/models/rsvp.php
+++ site/models/rsvp.php
@@ -4,5 +4,25 @@
 // In general the class name is {{ProjectFileName}}Page
 
 class RsvpPage extends Page {
-  // all methods of the Page class are inherited and can be overridden here now.
+    /**
+     * Creates and stores a new page
+     *
+     * @param array $props
+     * @return self
+     */
+    public static function create(array $props)
+    {
+      $collection = pages('rsvps')->children()->sortBy('uid', 'desc')->limit(1);
+
+      foreach ($collection as $post):
+        $id = $post->uid();
+        $last = $id+1;
+      endforeach;
+  
+      $props['slug'] = '_' . $last;
+      $props['content']['title'] = date('F jS, Y');
+      $props['content']['uid'] = $last;
+  
+      return parent::create($props);
+    }
 }
