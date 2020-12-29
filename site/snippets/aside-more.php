dec4f863b17f869f22c746f74df88a95bbaaf843
Calum Ryan <calumeryan@gmail.com>
2020-12-29 12:26:48 +0000

    Amend svg

--- site/snippets/aside-more.php
+++ site/snippets/aside-more.php
@@ -3,7 +3,7 @@
     <?php if ($site_menu = page('site-menu')): ?>
       <ul>
         <?php foreach ($site_menu->links()->toStructure() as $item): ?>
-        <li><a class="link icon-inline bg-fallback bg-<?= str_replace('/','',$item->url()) ?>" href="<?= $item->url() ?>"><?php if ( $item->icon()->isNotEmpty() ): ?><svg aria-hidden="true" class="icon" role="img" width="20" height="20"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlink:href="<?= $site->url() ?>/assets/icons/icons.sprite.svg#icon-<?= $item->icon() ?>"></use></svg><?php endif; ?><?= $item->title() ?></a></li>
+        <li><a class="link icon-inline bg-fallback bg-<?= str_replace('/','',$item->url()) ?>" href="<?= $item->url() ?>"><?php if ( $item->icon()->isNotEmpty() ): ?><svg aria-hidden="true" focusable="false" class="icon" role="img" width="20" height="20"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlink:href="<?= $site->url() ?>/assets/icons/icons.sprite.svg#icon-<?= $item->icon() ?>"></use></svg><?php endif; ?><?= $item->title() ?></a></li>
         <?php endforeach ?>
         </ul>
     <?php endif ?>
