3b4669e7d08979bffdf77c0e086c69122fb5124c
Calum Ryan <calumeryan@gmail.com>
2021-01-02 16:03:17 +0000

    Update like model

--- site/models/like.php
+++ site/models/like.php
@@ -1,8 +1,26 @@
 <?php
 
-// For a content file called `project.txt`
-// In general the class name is {{ProjectFileName}}Page
+class LikePage extends Page
+{
+    /**
+     * Creates and stores a new page
+     *
+     * @param array $props
+     * @return self
+     */
+    public static function create(array $props)
+    {
+      $collection = pages('likes')->children()->sortBy('uid', 'desc')->limit(1);
 
-class likePage extends Page {
-  // all methods of the Page class are inherited and can be overridden here now.
+      foreach ($collection as $post):
+        $id = $post->uid();
+        $last = $id+1;
+      endforeach;
+  
+      $props['slug'] = '_' . $last;
+      $props['content']['title'] = date('F jS, Y');
+      $props['content']['uid'] = $last;
+  
+      return parent::create($props);
+    }
 }
