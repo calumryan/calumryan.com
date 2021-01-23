01c37ae3a73cd933ef4d3292c61c084b640a06ab
Calum Ryan <calumeryan@gmail.com>
2021-01-23 16:07:17 +0000

    Experimental navigation

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -51,6 +51,18 @@
 
         </div>
 
+        <nav aria-label="website" class="header__navigation nav-horizontal">
+          <?php if ($site_menu = page('site-menu')): ?>
+          <ul>
+            <?php foreach ($site_menu->links()->toStructure() as $item): ?>
+            <?php if ( $item->special()->isNotEmpty() ) : ?>
+            <li><a class="link icon-inline bg-fallback bg-<?= str_replace('/','',$item->url()) ?>" href="<?= $item->url() ?>"><?php if ( $item->icon()->isNotEmpty() ): ?><svg aria-hidden="true" focusable="false" class="icon" role="img" width="20" height="20"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlink:href="<?= $site->url() ?>/assets/icons/icons.sprite.svg#icon-<?= $item->icon() ?>"></use></svg><?php endif; ?><?= $item->title() ?></a></li>
+            <?php endif ?>
+            <?php endforeach ?>
+          </ul>
+          <?php endif ?>
+        </nav>
+
         <?php snippet('intro') ?>
       </div>
     </div>
