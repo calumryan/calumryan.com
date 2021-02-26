5688a9ab4916727ffe636173bc6864b0b6466169
Calum Ryan <calumeryan@gmail.com>
2021-02-26 08:07:09 +0000

    Add bookshelf

---
+++ site/models/book.php
@@ -0,0 +1,3 @@
+<?php
+class bookPage extends Page {
+}
---
+++ site/templates/bookshelf.php
@@ -0,0 +1,36 @@
+<?php snippet('header') ?>
+
+<main id="content" class="page">
+
+  <div>
+
+    <section>
+    <?php foreach ($notes = $page->children()->listed()->sortBy('date', 'desc')->paginate(1) as $note): ?>
+
+    <?php if ( $note->feature_images()->isNotEmpty() ) : ?>
+    <picture class="shelf">
+        <?php $images = $note->feature_images()->toFiles(); foreach($images as $image): ?>
+        <img loading="lazy" class="u-photo" src="<?= $image->resize(200,300)->url() ?>" alt="<?= $image->alt() ?>" width="200" height="300">
+        <?php endforeach; ?>
+    </picture>
+    <?php endif ?>
+
+    <?php endforeach ?>
+    </section>
+
+    <div class="notes">
+    <?php foreach ($notes = $page->children()->listed()->sortBy('date', 'desc')->paginate(20) as $note): ?>
+    <?php 
+          $content_type = $note->parent();
+          $snippet_path = 'content-book'; 
+          snippet($snippet_path,['post' => $note]);
+    ?>
+    <?php endforeach ?>
+    </div>
+  <div>
+    
+  <?php snippet('pagination-book',['notes' => $notes]); ?>
+
+</main>
+
+<?php snippet('footer') ?>
