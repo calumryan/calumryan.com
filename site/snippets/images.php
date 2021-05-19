398c687c500721dbda05793ce0a43d017dd7797e
Calum Ryan <calumeryan@gmail.com>
2021-05-19 11:40:55 +0100

    Fix missing alt text on group images

--- site/snippets/images.php
+++ site/snippets/images.php
@@ -5,7 +5,7 @@
   foreach($images as $image): 
   ?>
   <a href="<?= $image->url() ?>">
-    <img loading="lazy" class="u-photo" src="<?= $image->resize(800)->url() ?>" alt="<?= $image->image_alt() ?>" width="300" height="300">
+    <img loading="lazy" class="u-photo" src="<?= $image->resize(800)->url() ?>" alt="<?= $image->alt() ?>" width="300" height="300">
   </a>
   <?php endforeach ?>
 </div>
