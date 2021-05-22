50b6ff2e76792640be36b7d69e3d2a6efc48ff61
Calum Ryan <calumeryan@gmail.com>
2021-05-22 11:26:08 +0100

    fix/Amend gallery focus state

--- site/models/bookmark.php
+++ site/models/bookmark.php
@@ -1,4 +1,23 @@
 <?php
 class bookmarkPage extends Page {
+    /**
+     * Creates and stores a new page
+     *
+     * @param array $props
+     * @return self
+     */
+    public static function create(array $props)
+    {
+      $collection = pages('bookmarks')->children()->sortBy('uid', 'desc')->limit(1);
 
+      foreach ($collection as $post):
+        $id = $post->uid();
+        $last = $id+1;
+      endforeach;
+  
+      $props['slug'] = '_' . $last;
+      $props['content']['uid'] = $last;
+  
+      return parent::create($props);
+    }
 }
--- site/snippets/post-meta.php
+++ site/snippets/post-meta.php
@@ -1,6 +1,6 @@
-<div hidden class="h-card p-author"><a class="p-name u-url" rel="author" href="https://calumryan.com/">Calum Ryan</a><img class="u-photo" src="<?= $site->url() ?>/assets/images/icon.png?v20200704"></div>
+<div hidden><a class="u-author" rel="author" href="https://calumryan.com/">Calum Ryan</a></div>
 <?php if ( $post->syndication()->isNotEmpty() ) : ?><a class="u-syndication" rel="noopener" href="<?= $post->syndication() ?>" hidden>Swarm</a><?php endif ?>
 <?php snippet('published', ['post' => $page]) ?>
 <?php if ($page->tags()->isNotEmpty()) : ?>
-<div hidden><?php snippet('tags', ['page' => $page]) ?></div>
+<div hidden><?php snippet('tags', ['page' => $page]) ?><a href="<?= $page->url() ?>" class="u-url"><?= $page->url() ?></a></div>
 <?php endif ?>
