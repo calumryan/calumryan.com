a997dd6ead77ae03ca5070c2c5093f1721ea1388
Calum Ryan <calumeryan@gmail.com>
2021-06-25 09:56:25 +0100

    Add syndication field to articles

--- site/snippets/post-meta.php
+++ site/snippets/post-meta.php
@@ -1,5 +1,5 @@
 <div hidden><a class="u-author" rel="author" href="https://calumryan.com/">Calum Ryan</a></div>
-<?php if ( $post->syndication()->isNotEmpty() ) : ?><a class="u-syndication" rel="noopener" href="<?= $post->syndication() ?>" hidden>Swarm</a><?php endif ?>
+<?php if ( $post->syndication()->isNotEmpty() ) : ?><a class="u-syndication" rel="noopener" href="<?= $post->syndication() ?>" hidden></a><?php endif ?>
 <?php snippet('published', ['post' => $page]) ?>
 <?php if ($page->tags()->isNotEmpty()) : ?>
 <div hidden><?php snippet('tags', ['page' => $page]) ?><a href="<?= $page->url() ?>" class="u-url"><?= $page->url() ?></a></div>
