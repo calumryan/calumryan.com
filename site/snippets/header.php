d153129970e32a3dc2c51a3a252ccf73eca1ad18
Calum Ryan <calumeryan@gmail.com>
2021-07-24 09:32:45 +0100

    Version bump

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -21,7 +21,7 @@
   <title><?= $page->title() ?> - <?= $site->title() ?></title>
   <?php endif ?>
 
-  <link href="<?= $site->url() ?>/assets/css/main.css?v20210522" rel="stylesheet">
+  <link href="<?= $site->url() ?>/assets/css/main.css?v20210724" rel="stylesheet">
 
   <?php  ?>
 
