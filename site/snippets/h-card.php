0c5be1902a10ffeea54f13993d2d4bc8a325e006
Calum Ryan <calumeryan@gmail.com>
2022-01-26 18:11:43 +0000

    Restore heading welcome message

--- site/snippets/h-card.php
+++ site/snippets/h-card.php
@@ -1,17 +1,16 @@
 <?php snippet('profile') ?>
 <div>
-    <h1>Happy New Year</h1>
-<div>
 <?php 
-// $Hour = date('G');
-// if ( $Hour >= 5 && $Hour <= 11 ) {
-//     echo "<h1>Good Morning</h1>";
-// } else if ( $Hour >= 12 && $Hour <= 18 ) {
-//     echo "<h1>Good Afternoon</h1>";
-// } else if ( $Hour >= 19 || $Hour <= 4 ) {
-//     echo "<h1>Good Evening</h1>";
-// }
+$Hour = date('G');
+if ( $Hour >= 5 && $Hour <= 11 ) {
+    echo "<h1>Good Morning</h1>";
+} else if ( $Hour >= 12 && $Hour <= 18 ) {
+    echo "<h1>Good Afternoon</h1>";
+} else if ( $Hour >= 19 || $Hour <= 4 ) {
+    echo "<h1>Good Evening</h1>";
+}
 ?>
+<div>
 <div class="p-note">
 <?= $page->special()->kt() ?>
 </div>
