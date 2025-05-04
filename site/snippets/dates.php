837174dfc8ed11cf311523b0ad66fed4744fe0ff
Calum Ryan <calumeryan@gmail.com>
2025-05-04 12:26:53 +0200

    Add year to event calendar header

--- site/snippets/dates.php
+++ site/snippets/dates.php
@@ -5,11 +5,13 @@
     <div class="date-block">
         <div class="month"><?=$page->date_start()->toDate('F')?></div>
         <div class="day"><?=$page->date_start()->toDate('j')?></div>
+        <div class="year"><?=$page->date_start()->toDate('Y')?></div>
     </div>
     <?php if ( $day1 != $day2 ) : ?>
     <div class="date-block date-block__end">
         <div class="month"><?=$page->date_end()->toDate('F')?></div>
         <div class="day"><?=$page->date_end()->toDate('j')?></div>
+        <div class="year"><?=$page->date_end()->toDate('Y')?></div>
     </div>
     <?php endif ?>
 </div>
