831c4a81337257d3e2156591398d6893bba12df6
Calum Ryan <calumeryan@gmail.com>
2025-05-04 13:28:15 +0200

    Add more alt text support for older posts and new using Kirby image alt field

--- site/snippets/content-articles.php
+++ site/snippets/content-articles.php
@@ -7,9 +7,9 @@
       <?php if ( $post->description()->isNotEmpty() ) : ?><p class="p-summary"><?= $post->description(); ?></p><?php endif; ?>
     </div>
     <?php if ( $image = $post->og_thumbnail()->toFile() ) : ?>
-    <img src="<?= $image->resize(150,150,100)->url() ?>" alt="<?= $post->og_thumbnail_description(); ?>" loading="lazy">
+    <img src="<?= $image->resize(150,150,100)->url() ?>" alt="<?= $image->alt(); ?>" loading="lazy">
     <?php elseif ( $post->thumbnail()->isNotEmpty() ) : ?>
-    <img src="<?= $post->thumbnail() ?>" alt="<?= $post->og_thumbnail_description(); ?>" loading="lazy">
+    <img src="<?= $post->thumbnail() ?>" alt="<?= $post->thumbnail_text() ?>" loading="lazy">
     <?php endif ?>
   </header>
   <?php snippet('content-footer',['post' => $post]) ?>
