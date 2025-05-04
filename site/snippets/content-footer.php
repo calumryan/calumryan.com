ac36e8556f116c9b6141d2835c3bba5808edcd96
Calum Ryan <calumeryan@gmail.com>
2025-05-04 12:50:07 +0200

    Remove duplicate link for articles and add thumb alt text

--- site/snippets/content-footer.php
+++ site/snippets/content-footer.php
@@ -2,13 +2,17 @@
   <div>
   <?php snippet('speaker-small', ['page' => $post]) ?>
   </div>
+  <?php if ( $post->parents() == 'articles') :?>
+  <?php else: ?>
   <a href="<?= $post->url() ?>" class="u-url link">
+  <?php endif ?>
     <time
       datetime="<?= $post->date()->toDate('Y-m-d\TH:i:sP') ?>"
       class="dt-published"
       ><?= $post->date()->toDate('j F Y') ?> at
       <?= $post->date()->toDate('H:i T') ?></time
-    ></a
+    ><?php if ( $post->parents() == 'articles') :?><?php else: ?></a
   >
+  <?php endif ?>
   <div hidden class="h-card p-author"><a class="p-name u-url" rel="author" href="https://calumryan.com/">Calum Ryan</a><img class="u-photo" src="<?= $site->url() ?>/assets/images/icon.png?v20250404" alt=""></div>
 </footer>
