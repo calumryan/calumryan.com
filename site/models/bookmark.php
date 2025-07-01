4cac0ae303d1d11c6d99e0ece4f23a1f43342cea
Calum Ryan <calumeryan@gmail.com>
2025-07-01 19:44:34 +0100

    Update models and blueprints

--- site/models/bookmark.php
+++ site/models/bookmark.php
@@ -1,23 +1,37 @@
 <?php
-class bookmarkPage extends Page {
+
+use Kirby\Cms\Page;
+
+class BookmarkPage extends Page
+{
     /**
-     * Creates and stores a new page
+     * Creates and stores a new bookmark page with incremented numeric UID
      *
      * @param array $props
      * @return self
      */
-    public static function create(array $props)
+    public static function create(array $props): self
     {
-      $collection = page('bookmarks')->children()->sortBy('uid', 'desc')->limit(1);
+        $bookmarksPage = page('bookmarks');
+        $children = $bookmarksPage ? $bookmarksPage->children() : null;
 
-      foreach ($collection as $post):
-        $id = $post->uid();
-        $last = $id+1;
-      endforeach;
-  
-      $props['slug'] = '_' . $last;
-      $props['content']['uid'] = $last;
-  
-      return parent::create($props);
+        $maxUid = 0;
+
+        if ($children && $children->count() > 0) {
+            foreach ($children as $child) {
+                // Get uid from content, cast to int, fallback 0
+                $uid = (int) $child->content()->get('uid')->value();
+                if ($uid > $maxUid) {
+                    $maxUid = $uid;
+                }
+            }
+        }
+
+        $newUid = $maxUid + 1;
+
+        $props['slug'] = '_' . $newUid;
+        $props['content']['uid'] = $newUid;
+
+        return parent::create($props);
     }
 }
